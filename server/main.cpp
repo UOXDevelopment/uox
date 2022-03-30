ef7a0a941e949830cb61187fddaf3faf94b7f62b
Charles Kerr <punt1959@users.noreply.github.com>
2022-03-30 14:35:45 -0400

    main

---
+++ server/main.cpp
@@ -0,0 +1,24 @@
+//
+// Copyright © 2022 Charles Kerr. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <stdexcept>
+#include <filesystem>
+#include <thread>
+#include <algorithm>
+
+#include "serverlocation.hpp"
+
+
+using namespace std::string_literals ;
+int main(int argc, const char * argv[]) {
+	auto config_file  = std::filesystem::path("uox.cfg");
+	if (argc > 1){
+		config_file = std::filesystem::path(argv[1]) ;
+	}
+	
+	return 0;
+}
